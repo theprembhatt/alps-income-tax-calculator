b0cf5acd3a69a19daaa634f9ffe0d3c9fd14034a
theprembhatt <153282504+theprembhatt@users.noreply.github.com>
2023-12-13 16:07:40 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,28 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
       event.preventDefault();
   
-      const age = parseInt(document.getElementById('age').value) || 0;
       const annualPremiumSelf = parseInt(document.getElementById('annual-premium-self').value) || 0;
       const annualPremiumParents = parseInt(document.getElementById('annual-premium-parents').value) || 0;
   
-      let maxDeductionSelf = 0;
-      let maxDeductionParents = 0;
-      let maxDeduction;
+      // Maximum deduction for "Annual Premium Paid for Self and Family" is 25000
+      const maxDeductionSelf = Math.min(25000, annualPremiumSelf);
   
-      if (age >= 60) {
-        // Senior Citizen
-        maxDeductionSelf = Math.min(25000, annualPremiumSelf);
-        maxDeductionParents = Math.min(25000, annualPremiumParents);
-      } else {
-        // Non-Senior Citizen
-        maxDeductionSelf = Math.min(25000, annualPremiumSelf);
-        maxDeductionParents = Math.min(25000, annualPremiumParents);
-      }
+      // Maximum deduction for "Annual Premium Paid for Parents" is 50000
+      const maxDeductionParents = Math.min(50000, annualPremiumParents);
   
       // Total maximum deduction is 50000
-      maxDeduction = maxDeductionSelf + maxDeductionParents;
-  
-      const totalDeductions = maxDeduction;
+      const totalDeductions = maxDeductionSelf + maxDeductionParents;
   
       resultContainer.innerHTML = `<p>Total Deductions: ${totalDeductions}</p>`;
     });
