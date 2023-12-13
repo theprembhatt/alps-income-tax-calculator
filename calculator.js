ccbb5c9e6ca16c8fcd934f99a4426319310bf0a4
theprembhatt <153282504+theprembhatt@users.noreply.github.com>
2023-12-13 16:17:08 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('deduction-form');
+    const totalDeductionsSpan = document.getElementById('total-deductions');
+    const taxSavingsSpan = document.getElementById('tax-savings');
     const resultContainer = document.getElementById('result-container');
     
     form.addEventListener('submit', function(event) {
@@ -7,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
       const annualPremiumSelf = parseInt(document.getElementById('annual-premium-self').value) || 0;
       const annualPremiumParents = parseInt(document.getElementById('annual-premium-parents').value) || 0;
+      const incomeBracket = document.getElementById('income-bracket').value;
   
       // Maximum deduction for "Annual Premium Paid for Self and Family" is 25000
       const maxDeductionSelf = Math.min(25000, annualPremiumSelf);
@@ -17,7 +20,24 @@ document.addEventListener('DOMContentLoaded', function() {
       // Total maximum deduction is 50000
       const totalDeductions = maxDeductionSelf + maxDeductionParents;
   
-      resultContainer.innerHTML = `<p>Total Deductions: ${totalDeductions}</p>`;
+      let taxSavingsPercentage;
+  
+      if (incomeBracket === '5-lakh-to-10-lakh') {
+        taxSavingsPercentage = 20.8;
+      } else if (incomeBracket === 'more-than-10-lakh') {
+        taxSavingsPercentage = 31.2;
+      } else {
+        // Default tax savings percentage if the income bracket is not specified
+        taxSavingsPercentage = 0;
+      }
+  
+      const taxSavings = (totalDeductions * taxSavingsPercentage) / 100;
+  
+      totalDeductionsSpan.textContent = totalDeductions;
+      taxSavingsSpan.textContent = taxSavings;
+  
+      // Display the results
+      resultContainer.style.display = 'block';
     });
   });
   
